307fd4e35bc9841d16297ebe6c350e428ad9afaa
Amiya Behera <amiyatulu@gmail.com>
2018-10-08 09:29:01 +0530

    generate_nodes

--- generate_network.py
+++ generate_network.py
@@ -2,17 +2,37 @@
 from itertools import combinations
 labels = ['label0', 'label1', 'label2','label3']
 mainlabels = ['teacher','student','content','building' ]
-cdata = []
-for n in range(1,5):
-	data = combinations(labels, n)
-	cdata.extend(data)
+## Generation of labels
+all_labels = []
+c = 0
+for mlb in mainlabels:
+	lb = []
+	for x in range(4):
+		lb.append('label' + str(c))
+		c = c + 1
+	all_labels.append(lb)
 
+
+
+all_cdata = []
+for labels in all_labels:
+	cdata = []
+	for n in range(1,5):
+		data = combinations(labels, n)
+		cdata.extend(data)
+	all_cdata.append(cdata)
+
+
+m = 0
 for mlb in mainlabels: 
-	for d in cdata:
+	y = 0
+	for d in all_cdata[m]:
 		s = list(d)
 		for x in range(30):
 			f = s.copy()
-			f.insert(0,mlb +str(x))
+			f.insert(0,mlb +str(y))
 			print(f)
+			y = y + 1
+	m = m + 1
 
 
